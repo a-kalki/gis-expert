baa9de4a3aa1aba938c86600a4518dac1604c315
NurGeo <anzpro@gmail.com>
2026-04-10 19:03:55 +0000

    обновлен и исправлен файл ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,18 @@
 const projectRoot = process.cwd();
 module.exports = {
   apps : [{
-    name: "it-course-landing", // Имя вашего приложения
-    script: "./src/app/server.ts",             // Исполняемый файл
-    interpreter: "bun",        // Указываем, что скрипт должен запускаться через Bun
-    args: "start:prod",        // Скрипт из package.json
-    cwd: projectRoot,          // Укажите абсолютный путь к корневой директории вашего проекта
-    instances: 1,              // Количество экземпляров приложения
-    autorestart: true,         // Автоматический перезапуск при сбое
-    watch: false,              // Отключить watch в продакшене
-    max_memory_restart: "1G",  // Перезапуск, если потребление памяти превысит 1GB
+    name: "gis-expert",
+    script: "/home/admin/.bun/bin/bun",
+    args: "src/app/server.ts",
+    cwd: projectRoot,
+    instances: 1,
+    exec_mode: "fork",
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
     env_production: {
-      NODE_ENV: "production",
-      PORT: "3101",
+      NODE_ENV: process.env.NODE_ENV ||  "production",
+      PORT: process.env.PORT || "3101",
     }
- }]
+  }]
 };
